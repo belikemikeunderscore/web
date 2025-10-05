fd3ac74f26a9e6b8b06e0e319e59760689ea6c30
mike <miguel.bras06@gmail.com>
2025-10-05 02:47:51 +0000

    this shit is messy as hell :sob: why did no one tell me to start working on this mobile first and THEN work on the desktop version :sob:

--- typing.js
+++ typing.js
@@ -1,8 +1,8 @@
   document.addEventListener('DOMContentLoaded', function () {
     var typed = new Typed('.typed-text', {
-      strings: ['emoção', 'convívio', 'ação', 'jogos', 'diversão', 'competição', 'intensidade', 'torneios'],  // list of words you want
+      strings: ['emoção', 'convívio', 'ação', 'jogos', 'diversão', 'competição', 'intensidade', 'torneios', ''],  // list of words you want
       typeSpeed: 100,        // ms per character typing
-      backSpeed: 100,         // ms per character deleting
+      backSpeed: 50,         // ms per character deleting
       backDelay: 1500,       // wait 1.5s before deleting
       loop: true,            // keep looping
       showCursor: true,
